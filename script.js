2bbdc0d06c247cc93555ac652880c91676435fcd
Depoule <jpoulet02100@gmail.com>
2019-01-24 13:41:37 +0100

    exo 5 fini et corrigé

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-function getNumber() {
-            // je declare que je vais chercher l'element "firstNumber" via l'Id et que je ne veut que la valeur, je stocke le tout dans firstNumber
-            var firstNumber = document.getElementById('firstNumber').value;
-            //  pareil pour secondNumber
-            var secondNumber = document.getElementById('secondNumber').value;
-            //  je déclare que result contient la multiplication de first et secondNumber
-            // sauf que je lui indique avec la fonction Math.trunc que je ne garde que le nombre premier de firstnumber
-            var result = Math.trunc(firstNumber) * secondNumber;
+// déclaration de la fonction
+function multiplyReal() {
+      var firstNumber = document.getElementById('firstNumber').value; // type string
+      var secondNumber = document.getElementById('secondNumber').value; // type string
+      //isNan(parameter) is Not a Number
+      // ! NOT//Sinon
+      // Condition
+      if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+        // On se sert du modulo pour déterminer si le nombre est à virgule. si le reste est différant de 0 alors c'est un nombre à virgule
+        if (Number(firstNumber) % 1 !== 0 && Number(secondNumber) % 1 !== 0) {
+          firstNumberInteger = Math.trunc(firstNumber);
+          result = firstNumberInteger * secondNumber;
+          alert(result);
 
-            alert(result);
-}
+        } else {
+          alert('ce sont des nombres entiers');
+        }
+      } else {
+        alert('attention ce ne sont pas des nombres');
+      }
+    }
